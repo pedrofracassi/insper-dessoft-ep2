2d43210f79bd9c76928e476de559b124ee4a2d59
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-11 23:14:53 -0300

    Mover funções pra um arquivo .py

--- baralho.py
+++ baralho.py
@@ -1,3 +1,15 @@
+def extrai_naipe(carta):
+    if len(carta)==3:
+        return carta[2]
+    else:    
+        return carta[1]
+
+def extrai_naipe(carta):
+    if len(carta)==3:
+        return carta[2]
+    else:    
+        return carta[1]
+
 def cria_baralho():
     baralho = ['2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','A♠','K♠','Q♠','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','A♥','K♥','Q♥','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','A♦','K♦','Q♦','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','A♣','K♣','Q♣']
     return baralho
--- função extrai valor
+++
@@ -1,5 +0,0 @@
-def extrai_valor(carta):
-    if len(carta)==3:
-        return carta[0]+carta[1]
-    else:
-        return carta[0]    
