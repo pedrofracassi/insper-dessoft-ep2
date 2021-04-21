2689a1c4ac62982160e131cf2b90fda0bcc1439b
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-04-21 16:45:46 -0300

    adc uma função

--- baralho.py
+++ baralho.py
@@ -25,3 +25,25 @@ def lista_movimentos_possiveis (baralho, i):
             movs_possiveis.append(mov)
 
     return movs_possiveis
+
+
+def nlembroonome(baralho):
+    listanaipe = []
+    for carta in baralho:
+        if carta[0]+carta[1] == "10":
+            if carta[2] not in listanaipe:
+                listanaipe.append(carta[2])
+        elif carta[1] not in listanaipe:
+            listanaipe.append(carta[1])
+        else:
+            return True 
+    listanumero = []
+    for carta in baralho:
+        if carta[0]+carta[1] == '10':
+            if carta[2] not in listanumero:
+                listanumero.append(carta[2])
+        elif carta[1] not in listanumero:
+            listanumero.append(carta[1])
+        else:
+            return True
+    return False      
