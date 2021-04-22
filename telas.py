a266c2998626521dcfb18ef4fe12c64d12f3cc82
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-22 14:50:10 -0300

    Cores!

--- telas.py
+++ telas.py
@@ -4,12 +4,12 @@ import colorama
 colorama.init()
 
 def imprime_logo ():
-  print("   ___           _                 _           _                     _                  ")
-  print("  / _ \__ _  ___(_) ___ _ __   ___(_) __ _    /_\   ___ ___  _ __ __| | ___  __ _  ___  ")
-  print(" / /_)/ _` |/ __| |/ _ \ '_ \ / __| |/ _` |  //_\\\\ / __/ _ \| '__/ _` |/ _ \/ _` |/ _ \ ")
-  print("/ ___/ (_| | (__| |  __/ | | | (__| | (_| | /  _  \ (_| (_) | | | (_| |  __/ (_| | (_) |")
-  print("\/    \__,_|\___|_|\___|_| |_|\___|_|\__,_| \_/ \_/\___\___/|_|  \__,_|\___|\__,_|\___/ ")
-  print("                                                                                        ")
+  print(colorama.Fore.RED, "   ___           _                 _       ", colorama.Fore.CYAN, "    _                     _                  ")
+  print(colorama.Fore.RED, "  / _ \__ _  ___(_) ___ _ __   ___(_) __ _ ", colorama.Fore.CYAN, "   /_\   ___ ___  _ __ __| | ___  __ _  ___  ")
+  print(colorama.Fore.RED, " / /_)/ _` |/ __| |/ _ \ '_ \ / __| |/ _` |", colorama.Fore.CYAN, "  //_\\\\ / __/ _ \| '__/ _` |/ _ \/ _` |/ _ \ ")
+  print(colorama.Fore.RED, "/ ___/ (_| | (__| |  __/ | | | (__| | (_| |", colorama.Fore.CYAN, " /  _  \ (_| (_) | | | (_| |  __/ (_| | (_) |")
+  print(colorama.Fore.RED, "\/    \__,_|\___|_|\___|_| |_|\___|_|\__,_|", colorama.Fore.CYAN, " \_/ \_/\___\___/|_|  \__,_|\___|\__,_|\___/ ")
+  print(colorama.Fore.RESET)
 
 def regras():
   utils.limpa_tela()
@@ -20,8 +20,9 @@ def regras():
 def menu():
   utils.limpa_tela()
   imprime_logo()
-  print('Escolha um modo de jogo')
-  print('')
+  print(f"{colorama.Back.CYAN}{colorama.Fore.BLACK}Escolha um modo de jogo")
+  print(colorama.Back.RESET, colorama.Fore.RESET)
   print(colorama.Fore.RED,  ' 1 - Texto (todas as cartas abertas)', colorama.Fore.RESET)
   print('  2 - ASCII')
   modo = input()
+  return modo
