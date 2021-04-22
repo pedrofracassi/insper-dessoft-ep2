79b205f4fa5409f2fbfc0d9d75f40806761ea8f7
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-22 15:10:26 -0300

    Renderização de cartas em ASCII

---
+++ render.py
@@ -0,0 +1,38 @@
+import baralho
+
+def carta_ascii (carta):
+  naipe = baralho.extrai_naipe(carta)
+  valor = baralho.extrai_valor(carta)
+
+  return [
+    '┌─────────┐',
+    '│         │',
+    '│         │',
+    '│         │',
+    '│    {}    │'.format(naipe),
+    '│         │',
+    '│         │',
+    '│         │',
+    '└─────────┘'
+  ]
+
+def linha_cartas (cartas):
+  cartas_ascii = []
+
+  for carta in cartas:
+    cartas_ascii.append(carta_ascii(carta))
+
+  linhas = []
+  
+  for carta in cartas_ascii:
+    for i, linha in enumerate(carta):
+
+      if len(linhas) < len(carta):
+        linhas.append("")
+
+      linhas[i] += linha
+
+  return linhas
+
+for a in linha_cartas(['2♠','3♠']):
+  print(a)
