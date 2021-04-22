38e75ba5e4b3afbea13755a9d743d04e35db447b
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-04-22 15:37:12 -0300

    modo1 mas executando meio certo

---
+++ modo1.py
@@ -0,0 +1,37 @@
+from baralho import cria_baralho
+from baralho import possui_movimentos_possiveis
+from baralho import lista_movimentos_possiveis
+from baralho import empilha
+x= cria_baralho()
+while possui_movimentos_possiveis(x):
+    i=1
+    for carta in x:
+        print('{}.'.format(i),carta)
+        i+=1
+    numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
+    if lista_movimentos_possiveis(x,(numero-1))==[]:
+        print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(1,len(x)))
+        numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
+    if len(lista_movimentos_possiveis(x,(numero-1))) > 0:
+        mov = lista_movimentos_possiveis(x,(numero-1))
+        if len(mov)==1:
+            novasequencia=empilha(x,(numero-1),mov[0])
+        elif len(mov)>1:
+            pergunta1 = int(input('Qual movimento deseja executar {} ou {}?'.format(mov[0],mov[1])))
+            novasequencia=(x,numero-1,mov[pergunta1])  
+           
+                  
+
+
+
+   
+
+
+
+
+
+
+
+
+
+    
