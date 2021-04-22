c1dc6831b3cba5170b41e7692abc5d330382efd8
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-04-22 14:49:46 -0300

    inseri as regras

--- telas.py
+++ telas.py
@@ -14,7 +14,21 @@ def imprime_logo ():
 def regras():
   utils.limpa_tela()
   imprime_logo()
-  print('pipipi popopo')
+  print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.")
+  print('Existem apenas dois movimentos possíveis:') 
+
+  print("1. Empilhar uma carta sobre a carta imediatamente anterior;") 
+  print("2. Empilhar uma carta sobre a terceira carta anterior.") 
+
+  print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:") 
+
+  print("1. As duas cartas possuem o mesmo valor ")
+  print('2. As duas cartas possuem o mesmo naipe.') 
+
+  print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.') 
+
+  print('Aperte [Enter] para iniciar o jogo...')
+
   input()
 
 def menu():
