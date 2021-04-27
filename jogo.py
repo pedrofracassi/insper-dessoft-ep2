e556df5fe8e39489f1ec31168aea98b238ddeab6
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-27 01:26:07 -0300

    Validar movimentos possíveis

--- jogo.py
+++ jogo.py
@@ -8,7 +8,7 @@ def input_validado (texto_primeiro, texto_erro, validacao, tuple_args):
 
   return resultado
 
-def valida_input_numero (resultado, min, max):
+def valida_input_numero (resultado, min, max, x):
   try:
     int(resultado)
   except:
@@ -30,12 +30,12 @@ def iniciar (renderiza_cartas):
           f'Escolha uma carta e digite um número entre {1} e {len(x)}: ',
           f'Número inválido. Escolha um número entre {1} e {len(x)}: ',
           valida_input_numero,
-          (1, len(x))
+          (1, len(x), x)
         ))
 
-        while baralho.lista_movimentos_possiveis(x,(numero-1))==[]:
-            print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(numero,len(x)))
-            numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
+        # while baralho.lista_movimentos_possiveis(x,(numero-1))==[]:
+        #    print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(numero,len(x)))
+        #    numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
         
         if len(baralho.lista_movimentos_possiveis(x,(numero-1))) > 0:
             mov = baralho.lista_movimentos_possiveis(x,(numero-1))
