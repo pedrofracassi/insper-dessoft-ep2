efe9b90dd9117f1b26fb22aa33eb51230ef2bcc7
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-26 23:55:52 -0300

    Melhorar imports

--- modo1.py
+++ modo1.py
@@ -1,24 +1,21 @@
-from baralho import cria_baralho
-from baralho import possui_movimentos_possiveis
-from baralho import lista_movimentos_possiveis
-from baralho import empilha
-from baralho import extrai_naipe
+import baralho
+
 import colorama
-x= cria_baralho()
+x= baralho.cria_baralho()
 def pinta_baralho(x):
     for carta in x:
-        if extrai_naipe(carta) == '♠':
+        if baralho.extrai_naipe(carta) == '♠':
             carta = colorama.Fore.RED, '{}'.format(carta),colorama.Fore.RESET
-        if extrai_naipe(carta)== '♦':
+        if baralho.extrai_naipe(carta)== '♦':
             carta = colorama.Fore.CYAN,'{}'.format(carta),colorama.Fore.RESET
-        if extrai_naipe(carta) == '♥':
+        if baralho.extrai_naipe(carta) == '♥':
             carta = colorama.Fore.MAGENTA,'{}'.format(carta),colorama.Fore.RESET
         else:
             carta = colorama.Fore.YELLOW,'{}'.format(carta),colorama.Fore.RESET                
     return x
-y= cria_baralho()
+y= baralho.cria_baralho()
 x = pinta_baralho(y)            
-while possui_movimentos_possiveis(x):
+while baralho.possui_movimentos_possiveis(x):
     i=1
     for carta in x:
         print('{}.'.format(i),carta)
@@ -27,22 +24,22 @@ while possui_movimentos_possiveis(x):
     if numero>len(x):
         while numero>len(x):
             numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
-    if lista_movimentos_possiveis(x,(numero-1))==[]:
+    if baralho.lista_movimentos_possiveis(x,(numero-1))==[]:
         print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(1,len(x)))
         numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
-    if len(lista_movimentos_possiveis(x,(numero-1))) > 0:
-        mov = lista_movimentos_possiveis(x,(numero-1))
+    if len(baralho.lista_movimentos_possiveis(x,(numero-1))) > 0:
+        mov = baralho.lista_movimentos_possiveis(x,(numero-1))
         if len(mov)==1:
-            novasequencia=empilha(x,(numero-1),mov[0])
+            novasequencia=baralho.empilha(x,(numero-1),mov[0])
         elif len(mov)>1:
-            print('Sobre qual carta você deseja empilhar? ')
+            print('Sobre qual carta você deseja baralho.empilhar? ')
             c0=x[numero-1]
             c1=x[numero-3]           
             print('Qual movimento deseja executar 1 ou 2?')
             print('1. {}'.format(c0)) 
             print('2. {}'.format(c1))
             pergunta1 = int(input(' '))
-            novasequencia=empilha(x,numero-1,mov[pergunta1-1])  
+            novasequencia=baralho.empilha(x,numero-1,mov[pergunta1-1])  
     x=novasequencia       
 if len(x)>1:
     print('Que pena,você perdeu!Continue tentando')
