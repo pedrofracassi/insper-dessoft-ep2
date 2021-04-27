0fa9afcfbb7ac74e8a656c084c06ec669f759ab3
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-04-27 00:53:39 -0300

    correção do format

--- jogo.py
+++ jogo.py
@@ -12,7 +12,7 @@ def iniciar (renderiza_cartas):
             while numero>len(x):
                 numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
         while baralho.lista_movimentos_possiveis(x,(numero-1))==[]:
-            print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(1,len(x)))
+            print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(numero,len(x)))
             numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
         if len(baralho.lista_movimentos_possiveis(x,(numero-1))) > 0:
             mov = baralho.lista_movimentos_possiveis(x,(numero-1))
