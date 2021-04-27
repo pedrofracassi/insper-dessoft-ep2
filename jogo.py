b3fe5567d8e99ac24cdcb2f6080bc23d3eee5719
pedrofracassi <pedroluizfracassi@gmail.com>
2021-04-27 01:21:54 -0300

    Validação inputs

--- jogo.py
+++ jogo.py
@@ -1,19 +1,42 @@
 import baralho
 import utils
 
+def input_validado (texto_primeiro, texto_erro, validacao, tuple_args):
+  resultado = input(texto_primeiro)
+  while not validacao(resultado, *tuple_args):
+    resultado = input(texto_erro)
+
+  return resultado
+
+def valida_input_numero (resultado, min, max):
+  try:
+    int(resultado)
+  except:
+    return False
+
+  if int(resultado) > max or int(resultado) < min:
+    return False
+
+  return True
+
 def iniciar (renderiza_cartas):
     x = baralho.cria_baralho()
 
     while baralho.possui_movimentos_possiveis(x):
         utils.limpa_tela()
         renderiza_cartas(x)
-        numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
-        if numero>len(x):
-            while numero>len(x):
-                numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
+
+        numero = int(input_validado(
+          f'Escolha uma carta e digite um número entre {1} e {len(x)}: ',
+          f'Número inválido. Escolha um número entre {1} e {len(x)}: ',
+          valida_input_numero,
+          (1, len(x))
+        ))
+
         while baralho.lista_movimentos_possiveis(x,(numero-1))==[]:
             print('A carta {} não pode ser movida. Por favor, digite um número entre 1 e {} :'.format(numero,len(x)))
             numero=int(input('Escolha uma carta e digite um número entre {} e {}: '.format(1,len(x))))
+        
         if len(baralho.lista_movimentos_possiveis(x,(numero-1))) > 0:
             mov = baralho.lista_movimentos_possiveis(x,(numero-1))
             if len(mov)==1:
