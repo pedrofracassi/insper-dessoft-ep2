dd5d2f8e4f15599b292c3541fe980d59a3a76f8d
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-05-02 14:55:21 -0300

    Alteração nas mensagens

--- telas.py
+++ telas.py
@@ -16,17 +16,20 @@ def regras():
   imprime_logo()
   print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.")
   print('Existem apenas dois movimentos possíveis:') 
-
-  print("1. Empilhar uma carta sobre a carta imediatamente anterior;") 
-  print("2. Empilhar uma carta sobre a terceira carta anterior.") 
-
+  print('')
+  print("  1. Empilhar uma carta sobre a carta imediatamente anterior;") 
+  print("  2. Empilhar uma carta sobre a terceira carta anterior.") 
+  print('')
   print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:") 
-
-  print("1. As duas cartas possuem o mesmo valor ")
-  print('2. As duas cartas possuem o mesmo naipe.') 
-
+  print('')
+  print("  1. As duas cartas possuem o mesmo valor ")
+  print('  2. As duas cartas possuem o mesmo naipe.') 
+  print('')
   print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.') 
-
+  print('')
+  print('Cartas em', colorama.Fore.YELLOW, 'amarelo', colorama.Fore.RESET, 'têm movimentos possíveis')
+  print('Cartas em', colorama.Fore.CYAN, 'azul', colorama.Fore.RESET, 'são as últimas empilhadas')
+  print('')
   print('Aperte [Enter] para iniciar o jogo...')
 
   input()
